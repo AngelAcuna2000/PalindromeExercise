8a42d3da5537eccfdb8741ff0420ba58992e0d8e
Angel Acuna <AngelAcuna2000@gmail.com>
2023-06-05 13:11:00 -0400

    Exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsPalindrome(string word)
+        {
+            var charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed = new string(charArray);
+            var isPalindrome = word.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+            return isPalindrome;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("racecar", true)]
+        [InlineData("race", false)]
 
+        public void PalindomeTester(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
